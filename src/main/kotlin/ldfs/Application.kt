b4ba32b1000566ec7051fbd2a055eae4760dd9fc
space-enthusiast <jinding0829@gmail.com>
2024-04-23 09:02:58 +0900

    init: chunk server

---
+++ src/main/kotlin/ldfs/Application.kt
@@ -0,0 +1,12 @@
+package ldfs
+
+import io.ktor.server.application.*
+import ldfs.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
